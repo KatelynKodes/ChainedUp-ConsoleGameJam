95ebecd468b4252f2a115a02c8d9e2f7a54f8896
Katelyn <katelynwest225@gmail.com>
2021-10-08 23:41:40 -0500

    Set up base class methods
    Set up base class and program class

---
+++ ConsoleGameJam/ConsoleGameJam/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGameJam
+{
+    class Game
+    {
+        private bool _gameOver;
+
+        public void Run()
+        {
+            Start();
+            while (!_gameOver)
+            {
+                Draw();
+                Update();
+            }
+            End();
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        { 
+
+        }
+
+        private void End()
+        { 
+        }
+
+        private void Draw()
+        { 
+        }
+    }
+}
